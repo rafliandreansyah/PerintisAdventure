2e88e00fe064d4427a102a2d153db61696775baf
Rafli Andreansyah <rafliandreansyah957@gmail.com>
2020-04-23 22:34:28 +0700

    Design Home screen layout

--- app/src/main/java/com/azhara/perintisadventure/ui/home/HomeActivity.kt
+++ app/src/main/java/com/azhara/perintisadventure/ui/home/HomeActivity.kt
@@ -1,6 +1,7 @@
 package com.azhara.perintisadventure.ui.home
 
 import android.os.Bundle
+import android.view.View
 import com.google.android.material.bottomnavigation.BottomNavigationView
 import androidx.appcompat.app.AppCompatActivity
 import androidx.navigation.findNavController
@@ -8,6 +9,7 @@ import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.setupActionBarWithNavController
 import androidx.navigation.ui.setupWithNavController
 import com.azhara.perintisadventure.R
+import kotlinx.android.synthetic.main.activity_home.*
 
 class HomeActivity : AppCompatActivity() {
 
@@ -24,7 +26,26 @@ class HomeActivity : AppCompatActivity() {
                 R.id.navigation_home, R.id.navigation_booked, R.id.navigation_profile
             )
         )
-        setupActionBarWithNavController(navController, appBarConfiguration)
+//        setupActionBarWithNavController(navController, appBarConfiguration)
+        toolbar_home.setupWithNavController(navController, appBarConfiguration)
         navView.setupWithNavController(navController)
+
+        navController.addOnDestinationChangedListener{_, destination, _ ->
+            if (destination.id == R.id.navigation_home){
+                tv_title_toolbar.visibility = View.GONE
+                img_toolbar_home.visibility = View.VISIBLE
+            }
+            if (destination.id == R.id.navigation_booked){
+                tv_title_toolbar.visibility = View.VISIBLE
+                tv_title_toolbar.text = "Pesanan"
+                img_toolbar_home.visibility = View.GONE
+            }
+            if (destination.id == R.id.navigation_profile){
+                tv_title_toolbar.visibility = View.VISIBLE
+                tv_title_toolbar.text = "Profil"
+                img_toolbar_home.visibility = View.GONE
+            }
+        }
+
     }
 }
