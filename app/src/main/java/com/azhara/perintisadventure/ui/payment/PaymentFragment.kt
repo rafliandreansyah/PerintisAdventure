750ba33fe6bf53a1f4ad06c32f2b78ed74ee233b
Rafli Andreansyah <rafliandreansyah957@gmail.com>
2020-05-17 19:59:38 +0700

    Pembuatan dan Design Layout fragment_payment untuk pembayaran booking

---
+++ app/src/main/java/com/azhara/perintisadventure/ui/payment/PaymentFragment.kt
@@ -0,0 +1,24 @@
+package com.azhara.perintisadventure.ui.payment
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+
+import com.azhara.perintisadventure.R
+
+/**
+ * A simple [Fragment] subclass.
+ */
+class PaymentFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_payment, container, false)
+    }
+
+}
