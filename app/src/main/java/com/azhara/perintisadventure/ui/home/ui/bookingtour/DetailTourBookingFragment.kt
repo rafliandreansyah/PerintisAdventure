a27c091cd4343ea08a4d3d65e90a8aca2506016b
Rafli Andreansyah <rafliandreansyah957@gmail.com>
2020-07-02 21:05:44 +0700

    membuat layout news, menambah font dan design layout news list

--- app/src/main/java/com/azhara/perintisadventure/ui/home/ui/bookingtour/DetailTourBookingFragment.kt
+++ app/src/main/java/com/azhara/perintisadventure/ui/home/ui/bookingtour/DetailTourBookingFragment.kt
@@ -17,7 +17,7 @@ class DetailTourBookingFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_detail_booking_tour, container, false)
+        return inflater.inflate(R.layout.fragment_detail_booking_tour_success, container, false)
     }
 
 }
